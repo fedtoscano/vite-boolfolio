448aae852f0266dab481f432f05677d76bc44b34
fedtoscano <federicotoscano611@gmail.com>
2024-08-21 18:11:50 +0200

    creata la chiamata API

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,39 @@
 <script>
+import axios from 'axios';
+
 export default {
-data() {
-return {  
-}
-}}
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getProjectsData() {
+      axios.get('http://127.0.0.1:8000/api/projects', {
+        params: {
+          // You can add query parameters here if needed
+        }
+        })
+        .then(response => {
+            console.log('ciao');
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+        .finally(() => {
+            // always executed
+        });
+    }
+  },
+  mounted() {
+    this.getProjectsData();
+  }
+};
 </script>
 
 <template>
     <h1>AppMain</h1>
-
+    
 </template>
 
 <style scoped>
