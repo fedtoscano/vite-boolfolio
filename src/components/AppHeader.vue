309f9b119976ef1943930e481a52be1a6e7bbc65
fedtoscano <federicotoscano611@gmail.com>
2024-08-22 12:51:26 +0200

    creata bozza dell-header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,33 @@
 <script>
 export default {
 data() {
-return {  
+return { 
+    navLinks: [
+        'Home', 'About', 'Blog'
+    ] 
 }
 }}
 </script>
 
 <template>
-    <h1>AppHeader</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1>MY PROJECTS PORTFOLIO</h1>
+        </div>
+        <div class="col-6">
+            <nav>
+                <ul>
+                    <li v-for="navLink, index in navLinks">
+                        {{ navLink }}
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/AppHeader.scss';
 </style>
